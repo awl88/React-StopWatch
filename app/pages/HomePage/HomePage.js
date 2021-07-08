a064b5f330ded281c7e5a4ba80467aacb52ce102
Andrew Lyon <anlyon99@gmail.com>
2021-07-08 12:06:21 -0600

    init commit

--- app/pages/HomePage/HomePage.js
+++ app/pages/HomePage/HomePage.js
@@ -1,52 +1,86 @@
-import React from 'react'
-import { Helmet } from 'react-helmet'
-import { FormattedMessage } from 'react-intl'
+import React, { useState } from 'react'
 import PropTypes from 'prop-types'
 
 // Components
 import { Box } from 'components/Box'
 import { Button } from 'components/Button'
 import { Container } from 'components/Container'
-import { Heading } from 'components/Heading'
-import { LogoHeader } from 'components/LogoHeader'
 import { Text } from 'components/Text'
 
-// Messages
-import messages from './HomePage.messages'
+// Helpers
+import { formatTimeText } from '../../utils/helpers'
 
 /*
- * HomePage
+ * StopWatch
  *
- * The HomePage renders:
- * - The page title and metadata using Helmet
- * - The page header and description
+ * I did a setInterval, but thought that a Date.now() - time started type solution
+ * might be better as it would be more accurate to times. I spent a few minutes trying
+ * to get it to work, but was unable to get it to work as I wanted so I went back to
+ * the setInterval method. Given more time I would most likely use Date.now() for this
+ * type of project. I was able to get laps working just in the nick of time as well.
+ *
+ * Given more time I would most likely break this up into separate components for the
+ * time, buttons, and laps. For this short project I did not use Redux at all, but if
+ * the times were needed elsewhere in the application I would add the laps to Redux if
+ * needed.
  *
  */
-export const HomePage = ({ history }) => (
-  <>
-    <Helmet>
-      <title>Home Page</title>
-      <meta name="description" content="A React.js Boilerplate application homepage" />
-    </Helmet>
+export const HomePage = ({ history }) => {
+  const [time, setTime] = useState(0)
+  const [isActive, setIsActive] = useState(false)
+  const [intervalId, setIntervalId] = useState(null)
+  const [laps, setLaps] = useState([])
 
-    <LogoHeader />
+  const startTimer = () => {
+    if (!isActive) {
+      setIsActive(true)
+      const interval = setInterval(() => {
+        setTime(time => time + 10)
+      }, 10)
+      setIntervalId(interval)
+    }
+  }
 
-    <Container align="center">
-      <Heading level="2" color="brand">
-        <FormattedMessage {...messages.startProjectHeader} />
-      </Heading>
+  const stopTimer = () => {
+    if (isActive) {
+      clearIntervalId()
+    }
+  }
 
-      <Text>
-        <FormattedMessage {...messages.startProjectMessage} />
-      </Text>
+  const resetTimer = () => {
+    setTime(0)
+    clearIntervalId()
+    setIntervalId(null)
+  }
 
-      <Box gap="small" direction="row" pad="large">
-        <Button label="Login" onClick={() => history.push('/login')} />
-        <Button label="Register" onClick={() => history.push('/register')} />
-      </Box>
-    </Container>
-  </>
-)
+  const addLap = () => {
+    setLaps([...laps, formatTimeText(time)])
+  }
+
+  const clearIntervalId = () => {
+    setIsActive(false)
+    clearInterval(intervalId)
+  }
+
+  return (
+    <>
+      <Container align="center">
+        <Text size="xlarge">{formatTimeText(time)}</Text>
+        <Box gap="small" direction="row" pad="small">
+          <Button label="Start" onClick={() => startTimer()} />
+          <Button label="Stop" onClick={() => stopTimer()} />
+        </Box>
+        <Box gap="small" direction="row" pad="small">
+          <Button label="Reset" onClick={() => resetTimer()} />
+          <Button label="Lap" onClick={() => addLap()} />
+        </Box>
+        {laps.map(lap => (
+          <Text key={lap}>{lap}</Text>
+        ))}
+      </Container>
+    </>
+  )
+}
 
 HomePage.propTypes = {
   history: PropTypes.object,
--- app/utils/helpers.js
+++ app/utils/helpers.js
@@ -76,3 +76,12 @@ export const updateArrayState = (state, { type, value, index }) => {
       return state
   }
 }
+
+export const formatTimeText = time => {
+  let miliseconds = `0${Math.floor(time / 10) % 100}`.slice(-2)
+  let seconds = `0${Math.floor(time / 1000) % 60}`.slice(-2)
+  let minutes = `0${Math.floor(time / 60000) % 60}`.slice(-2)
+  let hours = `0${Math.floor(time / 3600000)}`.slice(-2)
+
+  return `${hours}:${minutes}:${seconds}.${miliseconds}`
+}
